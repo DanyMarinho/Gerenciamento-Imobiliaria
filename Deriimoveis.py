8cf37abde63f46f3200c6e2bbac316c75bcaa349
DanyMarinho <cardosodanielly11@gmail.com>
2024-05-23 23:28:48 -0300

    Primeira fase do programa. 1.0

---
+++ Deriimoveis.py
@@ -0,0 +1,9 @@
+print('Deri imoveis')
+
+print('1 Cadastrar Imovel')
+print('2. Listar Imoveis')
+print('3. Ativar imovel')
+print('3. Sair')
+
+opcao_escolhida = input('Escolha uma opção:')
+print('Você escolheu a opção', opcao_escolhida)
